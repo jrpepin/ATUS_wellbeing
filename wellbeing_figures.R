778e7eabd10d01d0a3153a3a39fd4889f1670cbe
jrpepin <jpepin@prc.utexas.edu>
2019-04-03 10:34:34 -0500

    Update wellbeing_figures.R
    Fixed geom_text labels

--- wellbeing_figures.R
+++ wellbeing_figures.R
@@ -12,7 +12,7 @@ data$DOL    <- ordered(data$DOL,   levels = c("Other arrangement",  "Neither emp
                                               "Woman breadwinner &\nMan homemaker",   "Dual-earners ", "Man full-time &\nWoman part-time", 
                                               "Man breadwinner &\nWoman homemaker"))
 
-data$Gender <- ordered(data$Gender,   levels = c(" Women", " Men"))
+data$Gender <- ordered(data$Gender,   levels = c(" Men", " Women"))
 
 data$Emotion <- as_factor(data$Emotion)
 levels(data$Emotion)[levels(data$Emotion)=="Happy"]   <- "Happy\n"
@@ -26,13 +26,23 @@ data$Emotion <- ordered(data$Emotion,   levels = c("Happy\n", "Meaningful\n", "F
 ## Figures
 setwd("C:/Users/Joanna/Dropbox/Wellbeing/Figures")
 
+### Creating a separate dataset to indicate which labels to plot in geom_text
+labdata <- data %>%
+  mutate(
+    label = case_when(
+      Gender   == " Women"                                 ~ 1,
+      Emotion  == "Fatigued\n"                             ~ 1,
+      DOL      == "Woman breadwinner &\nMan homemaker"     ~ 1,
+      DOL      == "Other arrangement" & Emotion == "Meaningful\n" ~ 1))
+
 p1 <- data %>%
   filter(Emotion == "Happy\n" | Emotion == "Meaningful\n") %>%
   ggplot(aes(DOL, Value, fill = Gender, label = round(Value, 1))) +
   geom_point(size = 14, shape=21, alpha = 0.9) +
   facet_grid(. ~ Emotion) +
   coord_flip() +
-  geom_text(check_overlap = TRUE, color = "white", size = 7) +
+  geom_text(data=subset(labdata, label==1 & (Emotion == "Happy\n" | Emotion == "Meaningful\n")),
+            check_overlap = TRUE, color = "white", size = 7) +
   theme_minimal() +
   theme(axis.text.x      = element_text(vjust = 0.5, hjust=1, size = 18),
         strip.text.x     = element_text(face = "bold", size = 28),
@@ -46,9 +56,8 @@ p1 <- data %>%
         panel.grid.major.x = element_blank(),
         panel.border = element_blank()) +
   labs(x = " ", y = "\n", fill = "") +
-  scale_fill_manual(values=c("#6a5281", "#cf815b")) +
+  scale_fill_manual(values=c("#cf815b", "#6a5281")) +
   scale_y_continuous(limits = c(3, 5), breaks = NULL) 
-
 
 p2 <- data %>%
   filter(Emotion == "Fatigued\n") %>%
@@ -56,7 +65,8 @@ p2 <- data %>%
   geom_point(size = 14, shape=21, alpha = 0.9) +
   facet_grid(. ~ Emotion) +
   coord_flip() +
-  geom_text(check_overlap = TRUE, color = "white", size = 7) +
+  geom_text(data=subset(labdata, label==1 & Emotion == "Fatigued\n"),
+            check_overlap = FALSE, color = "white", size = 7) +
   theme_minimal() +
   theme(axis.text.x      = element_text(vjust = 0.5, hjust=1, size = 18),
         axis.text.y      = element_blank(),
@@ -71,8 +81,9 @@ p2 <- data %>%
         panel.grid.major.x = element_blank(),
         panel.border = element_blank()) +
   labs(x = " ", y = "\n", fill = "") +
-  scale_fill_manual(values=c("#6a5281", "#cf815b")) +
+  scale_fill_manual(values=c("#cf815b", "#6a5281")) +
   scale_y_continuous(limits = c(1, 3), breaks = NULL)
+
 
 p3 <- data %>%
   filter(Emotion == "Stressed\n" |Emotion == "Sad\n") %>%
@@ -80,7 +91,8 @@ p3 <- data %>%
   geom_point(size = 14, shape=21, alpha = 0.9) +
   facet_grid(. ~ Emotion) +
   coord_flip() +
-  geom_text(check_overlap = TRUE, color = "white", size = 7) +
+  geom_text(data=subset(labdata, label==1 & (Emotion == "Stressed\n" |Emotion == "Sad\n")),
+            check_overlap = TRUE, color = "white", size = 7) +
   theme_minimal() +
   theme(axis.text.x      = element_text(vjust = 0.5, hjust=1, size = 18),
         axis.text.y      = element_blank(),
@@ -95,7 +107,7 @@ p3 <- data %>%
         panel.grid.major.x = element_blank(),
         panel.border = element_blank()) +
   labs(x = " ", y = "\n", fill = "") +
-  scale_fill_manual(values=c("#6a5281", "#cf815b")) +
+  scale_fill_manual(values=c("#cf815b", "#6a5281")) +
   scale_y_continuous(limits = c(0, 2), breaks = NULL)
 
 poster <- grid.arrange(p1, p2, p3, ncol = 3, widths = c(2.4, .9, 1.5))
